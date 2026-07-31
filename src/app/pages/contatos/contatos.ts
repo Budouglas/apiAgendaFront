0eab9e522b4405d4339fe3f703b08ff931336b0c
Budouglas <d3500bueno@gmail.com>
2026-07-31 17:36:32 -0300

    feat: render contatos list from backend

--- src/app/pages/contatos/contatos.ts
+++ src/app/pages/contatos/contatos.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ContatosService } from '../../services/contatos';
 import { Contato} from '../../models/contato';
 
 @Component({
   selector: 'app-contatos',
-  imports: [],
   templateUrl: './contatos.html',
+  imports: [CommonModule],
   styleUrl: './contatos.css',
+  standalone: true,
 })
 export class Contatos implements OnInit {
-  private  contatoService = inject(ContatosService)
+  private contatoService = inject(ContatosService);
 
   contatos: Contato[] = [];
 
@@ -20,11 +22,13 @@ export class Contatos implements OnInit {
   loadContatos(): void {
     this.contatoService.findAll().subscribe({
       next: (response) => {
+        console.log('resposta api', response);
         this.contatos = response;
+        console.log('lista no componente', this.contatos);
       },
       error: (error) => {
         console.error('Erro ao Buscar Contatos', error);
-      }
+      },
     });
   }
 }
