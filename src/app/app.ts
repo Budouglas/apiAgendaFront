88b89e165b7fbb22bfecfaf2666d7eac4b5f8794
Budouglas <d3500bueno@gmail.com>
2026-07-31 17:34:54 -0300

    fix: componente

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 import {Contatos} from './pages/contatos/contatos';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Contatos],
+  imports: [Contatos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('agenda-front');
 }
