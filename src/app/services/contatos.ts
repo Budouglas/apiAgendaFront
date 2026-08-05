8f90a1bd00b33eb6dedc94d14d46e130791e87fd
Budouglas <d3500bueno@gmail.com>
2026-08-05 11:17:04 -0300

    feat: add constructor httpclient

--- src/app/services/contatos.ts
+++ src/app/services/contatos.ts
@@ -1,18 +1,21 @@
-import {Injectable, inject} from "@angular/core";
-import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Contato } from '../models/contato';
 
 @Injectable({
   providedIn: 'root',
 })
-
 export class ContatosService {
-  private http = inject(HttpClient);
-
   private apiUrl = 'http://localhost:8080/api/contatos';
+
+  constructor(private http: HttpClient) {}
 
   findAll(): Observable<Contato[]> {
     return this.http.get<Contato[]>(this.apiUrl);
+  }
+
+  save(contato: Omit<Contato, 'id'>): Observable<Contato> {
+    return this.http.post<Contato>(this.apiUrl, contato);
   }
 }
