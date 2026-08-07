b948998e1526243bb770c415223c5d5e0490b89c
Budouglas <d3500bueno@gmail.com>
2026-08-07 12:51:43 -0300

    fix: remove unused RouterLink import from app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { Header } from './core/component/header/header';
 import { Footer } from './core/component/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLink, RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
